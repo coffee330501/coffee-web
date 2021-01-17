a5a669255204098f723466de0b238c5e82c6ba2c
objcfeng <1034440274@qq.com>
2021-01-17 23:20:11 +0800

    创建项目

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
+console.log('server started at localhost:3000');
